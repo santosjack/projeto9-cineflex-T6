43228a5d0125cf9cabd0f9f6eb2651c29b9b809e
santosjack <jacksantos1408@gmail.com>
2022-05-23 16:30:57 -0300

    layout mobile ajustado

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,8 +6,8 @@ export default function Footer ({posterURL, title, weekday, date, time}) {
                 <img src={posterURL} alt="" />
             </div>
             <div className={!!title ? `text` : `hidden`}>
-                <h2>{title}</h2>
-                <span className={!!weekday ? `text` : `hidden`}>{weekday} - {time}</span>
+                <div>{title}</div>
+                <div className={!!weekday ? `text` : `hidden`}>{weekday} - {time}</div>
             </div>
         </div>
     )
